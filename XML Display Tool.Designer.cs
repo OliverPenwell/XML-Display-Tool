e9240a49cdafaee0260cc1c01219154474d6abaa
Oliver Penwell <oliverjpen@protonmail.ch>
2019-07-19 13:15:27 +0100

    Release 0.25 - One Button Release.  - When a file is selected the path is shown in Box 1 and the XML file printed in Box 2. - Very basic, but it works.

--- XML Display Tool.Designer.cs
+++ XML Display Tool.Designer.cs
@@ -32,7 +32,6 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -61,28 +60,17 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(12, 94);
+            this.textBox2.Location = new System.Drawing.Point(12, 51);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(776, 352);
+            this.textBox2.Size = new System.Drawing.Size(776, 395);
             this.textBox2.TabIndex = 3;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(12, 48);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(776, 40);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Display File";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.Button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
@@ -99,7 +87,6 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button2;
     }
 }
 
